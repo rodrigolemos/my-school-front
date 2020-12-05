2b317f402219ebaecf2f7aa90751d9b2584cbfb6
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-05 15:52:13 -0300

    Code: create course list as server side render page

--- src/pages/course-list.tsx
+++ src/pages/course-list.tsx
@@ -1,13 +1,56 @@
+import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
+import api from '../services/api'
 
-const CourseList = () => {
+interface ICourse {
+  id: string
+  name: string
+  description: string
+  period: string
+  updated_at: Date
+}
+
+interface CourseListProps {
+  courses: ICourse[]
+}
+
+export default function CourseList({ error, courses }) {
   const router = useRouter()
   return (
     <>
       <h1>Cursos</h1>
+      <p>{error}</p>
+      <ul>
+        {courses.map((course: ICourse) => (
+          <li key={course.id}>{course.name} - {course.description}</li>
+        ))}
+      </ul>
       <span onClick={() => router.back()}>Voltar</span>
     </>
   )
 }
 
-export default CourseList
+export const getServerSideProps: GetServerSideProps<any> = async () => {
+  try {
+    const response = await api.get<CourseListProps>('/courses')
+
+    if (response.status !== 200)
+      throw new Error()
+
+    const courses: CourseListProps = response.data
+
+    return {
+      props: {
+        courses
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      props: {
+        courses: [],
+        error: 'Não foi possível consultar os cursos.'
+      }
+    }
+  }
+}
