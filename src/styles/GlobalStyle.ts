e5aac4e84c9c6655a5e4b164846a766b133f2c25
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-05 11:48:38 -0300

    Config: use styled-components

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`
