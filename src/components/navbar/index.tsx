33a4688cce051ad22ccf3352cbba99152f93e6d0
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-06 19:18:59 -0300

    Code: responsive navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,57 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import { FiMoon } from 'react-icons/fi'
-import { Nav, NavContent, Title, Menu } from './styles'
+import { DesktopNav, NavContent, Title, MobileMenu, Menu, OpenMobileNav, CloseMobileNav, MobileNav } from './styles'
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+
   return (
-    <Nav>
-      <NavContent>
-        <Title>
-          <Link href="/">
-            My School
-          </Link>
-        </Title>
-        <Menu>
+    <>
+      <DesktopNav isMobile={isMobile}>
+        <NavContent>
+          <Title>
+            <Link href="/">
+              My School
+            </Link>
+          </Title>
+          <Menu>
+            <li>
+              <Link href="/course-list">
+                Cursos
+              </Link>
+            </li>
+            <li>
+              <Link href="/create-profile">
+                Criar Perfil
+              </Link>
+            </li>
+            <li>
+              <Link href="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                Sobre
+              </Link>
+            </li>
+            <li>
+              <FiMoon />
+            </li>
+          </Menu>
+          <OpenMobileNav onClick={() => setIsMobile(true)} />
+        </NavContent>
+      </DesktopNav>
+      <MobileNav isMobile={isMobile}>
+        <CloseMobileNav onClick={() => setIsMobile(false)} />
+        <MobileMenu>
+          <li>
+            <Link href="/">
+              Início
+            </Link>
+          </li>
           <li>
             <Link href="/course-list">
               Cursos
@@ -32,12 +72,9 @@ const Navbar = () => {
               Sobre
             </Link>
           </li>
-          <li>
-            <FiMoon />
-          </li>
-        </Menu>
-      </NavContent>
-    </Nav>
+        </MobileMenu>
+      </MobileNav>
+    </>
   )
 }
 
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -1,6 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
 
-export const Nav = styled.nav`
+interface NavProps {
+  isMobile: boolean
+}
+
+export const DesktopNav = styled.nav<NavProps>`
+  z-index: 1;
   position: fixed;
   top: 0;
   left: 0;
@@ -16,13 +23,82 @@ export const Nav = styled.nav`
   justify-content: center;
 `
 
-export const NavContent = styled.div`
-  width: 70%;
-  height: 100%;
+export const MobileNav = styled.div<NavProps>`
+  display: none;
+  z-index: 2;
+  position: fixed;
+  top: 0;
+  left: 0;
 
+  width: 100%;
+  height: 100vh;
+
+  background-color: var(--white);
+
+  ${({ isMobile }) => isMobile && css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
+`
+
+export const NavContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  width: 70%;
+  height: 100%;
+
+  // Tablets
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 0 2rem;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  
+  a, a:active, a:visited {
+    color: inherit;
+    text-decoration: none !important;
+  }
+`
+
+export const OpenMobileNav = styled(GiHamburgerMenu)`
+  display: none;
+  font-size: 3rem;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`
+
+export const CloseMobileNav = styled(AiOutlineClose)`
+  position: fixed;
+  top: 2rem;
+  right: 2rem;
+  font-size: 2.5rem;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
+`
+
+export const Menu = styled.ul`
+  display: flex;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  li {
+    list-style-type: none;
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin: 0 2rem;
+  }
 
   a, a:active, a:visited {
     text-decoration: none;
@@ -35,17 +111,29 @@ export const NavContent = styled.div`
   }
 `
 
-export const Title = styled.h1`
-  font-size: 3rem;
-`
-
-export const Menu = styled.ul`
+export const MobileMenu = styled.ul`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+
+  width: 100%;
+  height: 50%;
 
   li {
     list-style-type: none;
-    font-size: 1.5rem;
+    font-size: 2.7rem;
     font-weight: bold;
     margin: 0 2rem;
+  }
+
+  a, a:active, a:visited {
+    text-decoration: none;
+    color: inherit;
+    transition: all .2s ease-in-out;
+
+    &:hover {
+      color: var(--primary);
+    }
   }
 `
