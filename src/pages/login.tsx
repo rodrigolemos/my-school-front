63fc5003971484216e60fc4b363eb168ade5812f
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-09 16:31:04 -0300

    Code: login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,115 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
-import Navbar from '../components/navbar'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { CircularProgress } from '@material-ui/core'
+import { GoMortarBoard } from 'react-icons/go'
+import { IoIosArrowBack } from 'react-icons/io'
+import { VscUnlock } from 'react-icons/vsc'
+import { Container, DesktopPanel, FormPanel, BackButton } from '../styles/pages/login'
+import api from '../services/api'
 import Toast from '../components/toast'
 
+interface IFormInput {
+  email: string
+  password: string
+}
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(6),
+})
+
 const Login: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
   const router = useRouter()
+  const { register, handleSubmit, errors } = useForm<IFormInput>({
+    resolver: yupResolver(schema)
+  })
+
   const { user } = router.query
 
+  const onSubmit = async (data: IFormInput) => {
+    setLoading(true)
+    setError('')
+    try {
+      const response = await api.post('/users/create', data)
+      if (response.status === 201) {
+        router.push('/dashboard')
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status == 401) {
+          setError('Ops, não foi possível efetuar seu login. Por favor, confira suas informações e tente novamente.')
+        } else {
+          setError('Ops, não foi possível efetuar seu login. Por favor, tente novamente mais tarde.')
+        }
+      } else {
+        setError('Ops, houve alguma falha em nosso servidor. Por favor, tente novamente mais tarde.')
+      }
+    }
+    setLoading(false)
+  }
+
   return (
-    <>
-      <Navbar />
-      {user && <Toast type="success" message="Perfil criado com sucesso!" />}
-      <h1>Login</h1>
-      <span onClick={() => router.back()}>Voltar</span>
-    </>
+    <Container>
+      <FormPanel>
+        {error && <Toast type="error" message={error} />}
+        {user && <Toast type="success" message="Perfil criado com sucesso. Você já pode acessar a plataforma!" />}
+        <BackButton onClick={() => router.push('/')}>
+          <IoIosArrowBack /> Voltar
+        </BackButton>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1><GoMortarBoard />My School</h1>
+          <label>
+            E-mail
+            <input
+              type="email"
+              name="email"
+              minLength={3}
+              maxLength={50}
+              required
+              ref={register}
+            />
+            {errors.email && (
+              <p className="error">Preencha corretamente o email</p>
+            )}
+          </label>
+          <label>
+            Senha
+            <input
+              type="password"
+              name="password"
+              minLength={6}
+              maxLength={100}
+              autoComplete=""
+              required
+              ref={register}
+            />
+            {errors.password && (
+              <p className="error">Preencha corretamente a senha</p>
+            )}
+          </label>
+          {!loading ? (
+            <button>ENTRAR</button>
+          ) : (
+              <div className="loading">
+                <CircularProgress />
+              </div>
+            )}
+        </form>
+      </FormPanel>
+      <DesktopPanel>
+        <div className="overlay" />
+        <div className="brand">
+          <h2><GoMortarBoard />My School</h2>
+          <VscUnlock className="desktop" />
+          <h3>Efetue o login para acessar a plataforma</h3>
+        </div>
+      </DesktopPanel>
+    </Container>
   )
 }
 
---
+++ src/styles/pages/login.ts
@@ -0,0 +1,203 @@
+import styled from 'styled-components'
+import { showFromLeft } from '../animations'
+
+const backgroundImage = require('../../../public/images/typing-1.jpg')
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`
+
+export const DesktopPanel = styled.div`
+  width: 55vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--secondary);
+
+  background:
+    linear-gradient(rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)),
+    url("${backgroundImage}");
+  background-size: cover;
+
+  div.overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: var(--secondary);
+    opacity: .7;
+  }
+
+  div.brand {
+    z-index: 2;
+    opacity: .9;
+    width: 50%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: var(--white);
+
+    h2 {
+      display: flex;
+      align-items: center;
+      font-size: 3.8rem;
+      margin-bottom: 3rem;
+
+      svg {
+        margin-right: 1rem;
+      }
+    }
+
+    svg.desktop {
+      font-size: 27rem;
+      margin-bottom: 3rem;
+    }
+
+    h3 {
+      font-size: 2rem;
+      text-align: center;
+    }
+  }
+
+  @media (max-width: 998px) {
+    display: none;
+  }
+`
+
+export const BackButton = styled.div`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  svg {
+    margin-right: 1rem;
+    font-size: 1.7rem;
+  }
+`
+
+export const FormPanel = styled.div`
+  position: relative;
+  width: 45vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: var(--secondary);
+
+  @media (max-width: 998px) {
+    width: 100vw;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    animation: ${showFromLeft} .5s ease-in-out;
+
+    width: 60%;
+    min-height: 50%;
+
+    @media (max-width: 998px) {
+      width: 50vw;
+    }
+    
+    @media (max-width: 500px) {
+      width: 90vw;
+      height: 100%;
+    }
+
+    h1 {
+      font-size: 3.8rem;
+      margin-bottom: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        margin-right: 1rem;
+      }
+    }
+
+    label {
+      display: flex;
+      flex-direction: column;
+
+      width: 100%;
+      margin: .7rem 0;
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+
+    input {
+      height: 4rem;
+      padding: .5rem;
+      font-size: 1.5rem;
+      border: 2px solid var(--shadow);
+      outline: none;
+      border-radius: 5px;
+      margin-top: .5rem;
+      color: var(--tertiary-light);
+      font-weight: bold;
+      transition: all .2s ease-in-out;
+
+      ::placeholder {
+        font-weight: bold;
+        color: #CCC;
+      }
+
+      &:focus {
+        border: 2px solid var(--secondary-light);
+      }
+    }
+
+    p.error {
+      font-weight: normal;
+      font-size: 1.2rem;
+      color: #E00;
+      margin: .5rem 0;
+    }
+
+    button {
+      width: 100%;
+      margin-top: 2rem;
+      border: none;
+      outline: none;
+      border-radius: 5px;
+      height: 4rem;
+      cursor: pointer;
+      transition: all .2s ease-in-out;
+      
+      font-weight: bold;
+      background-color: var(--secondary);
+      color: var(--white);
+
+      &:focus, &:hover {
+        background-color: var(--secondary-light);
+      }
+    }
+
+    div.loading {
+      width: 100%;
+      text-align: center;
+      margin-top: 2rem;
+      height: 4rem;
+      
+      svg {
+        color: var(--secondary);
+      }
+    }
+  }
+`
