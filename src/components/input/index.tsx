3f0e480a8b6e0e529f4a9c55ddfc03285c572c23
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-09 15:59:29 -0300

    Code: create toast component

--- src/components/input/index.tsx
+++
@@ -1,9 +0,0 @@
-import { InputHTMLAttributes } from "react"
-
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
-  return (
-    <input {...props} />
-  )
-}
-
-export default Input
---
+++ src/components/toast/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Snackbar from '@material-ui/core/Snackbar'
+import { AlertProps } from '@material-ui/lab/Alert'
+import { StyledAlert } from './styles'
+
+const Alert = (props: AlertProps) => {
+  return <StyledAlert elevation={6} variant="filled" {...props} />;
+}
+
+interface IToast {
+  type: 'error' | 'warning' | 'success' | 'info'
+  message: string
+}
+
+const Toast: React.FC<IToast> = ({ type, message }: IToast) => {
+  const [open, setOpen] = useState<boolean>(message ? true : false);
+
+  const handleClose = (_?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpen(false)
+  }
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity={type}>
+        {message}
+      </Alert>
+    </Snackbar>
+  )
+}
+
+export default Toast
