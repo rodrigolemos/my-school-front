922cd6998e499bb4263aa00183d21d948b5f5074
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-11 16:52:13 -0300

    Code: add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { ThemeContextProvider } from '../hooks/theme'
 import GlobalStyle from '../styles/GlobalStyle'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeContextProvider>
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeContextProvider>
   )
 }
 
