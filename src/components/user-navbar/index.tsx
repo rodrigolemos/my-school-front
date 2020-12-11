c691e485cdca1214f3d5146e8e0580e1661cce80
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-11 14:15:41 -0300

    Code: add logout

--- src/components/user-navbar/index.tsx
+++ src/components/user-navbar/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Cookies } from 'react-cookie'
+import { useRouter } from 'next/router'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { IoMdNotificationsOutline } from 'react-icons/io'
 import { BsPower } from 'react-icons/bs'
@@ -6,7 +8,18 @@ import { MdChatBubbleOutline } from 'react-icons/md'
 import { DesktopNav, NavContent, Title, MobileMenu, Menu, OpenMobileMenu, OpenMobileNav, CloseMobileNav, MobileNav } from './styles'
 
 const UserNavbar: React.FC = () => {
+  const router = useRouter()
   const [isMobile, setIsMobile] = useState(false)
+
+  const logOut = () => {
+    const cookies = new Cookies()
+
+    cookies.remove('@my-school:email')
+    cookies.remove('@my-school:token')
+
+    router.push('/login')
+  }
+
   return (
     <>
       <DesktopNav isMobile={isMobile}>
@@ -25,7 +38,7 @@ const UserNavbar: React.FC = () => {
               <IoMdNotificationsOutline />
             </li>
             <li>
-              <BsPower />
+              <BsPower onClick={logOut} />
             </li>
           </Menu>
           <OpenMobileNav>
