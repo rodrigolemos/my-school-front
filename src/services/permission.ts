e2cde8efdb9d0fb1faa44dd8b40822a0bfd7e2bd
Rodrigo <rodrigolemosrl@hotmail.com>
2020-12-12 17:50:12 -0300

    Code: check permission

---
+++ src/services/permission.ts
@@ -0,0 +1,18 @@
+import api from './api'
+
+export const checkPermission = async (token: string, id: string): Promise<boolean> => {
+  try {
+    const response = await api.get(`/users/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    if (response.status !== 200)
+      throw new Error('Not allowed')
+
+    return true
+  } catch (error) {
+    return false
+  }
+}
