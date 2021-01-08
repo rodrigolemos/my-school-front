8dac716cdeb48f3effcaaa5d62c4daa2fc65a7a4
Rodrigo <rodrigolemosrl@hotmail.com>
2021-01-08 20:02:30 -0300

    Code: consume courses API

--- src/pages/course-detail/[slug].tsx
+++ src/pages/course-detail/[slug].tsx
@@ -1,21 +1,57 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { CircularProgress } from '@material-ui/core';
+import api from '../../services/api';
 
 import PublicLayout from '../../components/public-layout';
 import { Section, CourseInfo, CourseDescription } from '../../styles/pages/course-detail';
 
+interface ICourse {
+  id: string;
+  name: string;
+  description: string;
+  period: string;
+  updated_at: Date;
+}
+
 export default function CourseDetail(): ReactElement {
   const router = useRouter();
   const course_id = router.query.slug;
+  const [course, setCourse] = useState<ICourse>({} as ICourse);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchCourseDetail = async () => {
+    setLoading(true);
+
+    console.log(course_id);
+    const response = await api.get<ICourse>(`/courses/${course_id}`);
+
+    setLoading(false);
+
+    if (response.status !== 200) throw new Error();
+
+    setCourse(response.data);
+  };
+
+  useEffect(() => {
+    fetchCourseDetail();
+  }, []);
+
   return (
     <PublicLayout>
       <Section>
-        <CourseInfo>
-          <h1>Hello {course_id}</h1>
-        </CourseInfo>
-        <CourseDescription>
-          <h1>Hello {course_id}</h1>
-        </CourseDescription>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <CourseInfo>
+              <h1>Hello {course_id}</h1>
+            </CourseInfo>
+            <CourseDescription>
+              <h1>{JSON.stringify(course)}</h1>
+            </CourseDescription>
+          </>
+        )}
       </Section>
     </PublicLayout>
   );
