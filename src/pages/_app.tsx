98222dc69bcbbfa7463cd3801e1d0c3f8466c1bc
Rodrigo <rodrigolemosrl@hotmail.com>
2021-02-06 17:29:51 -0300

    Code: add header, meta and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import Head from 'next/head';
 import { ThemeContextProvider } from '../hooks/theme';
 import GlobalStyle from '../styles/GlobalStyle';
 
@@ -6,10 +7,14 @@ import GlobalStyle from '../styles/GlobalStyle';
 function MyApp({ Component, pageProps }): ReactElement {
   return (
     <ThemeContextProvider>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
-        rel="stylesheet"
-      />
+      <Head>
+        <title>My School</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeContextProvider>
