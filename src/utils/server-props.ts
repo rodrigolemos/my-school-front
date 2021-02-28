d5db1a6df812d69a521a320d48f1fb5b50ce2457
Rodrigo <rodrigolemosrl@hotmail.com>
2021-02-28 13:59:52 -0300

    Code: getServerSidePropsUser

--- src/utils/server-props.ts
+++ src/utils/server-props.ts
@@ -1,14 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { GetServerSideProps } from 'next';
 import { checkAuth } from '../services/auth';
 import { checkPermission } from '../services/permission';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getServerSidePropsAdmin: GetServerSideProps<unknown> = async (context: any) => {
   try {
     checkAuth(context.req.cookies['@my-school:token']);
     const { id, name } = JSON.parse(context.req.cookies['@my-school:user']);
     const isAdmin = await checkPermission(context.req.cookies['@my-school:token'], id);
     if (!isAdmin) throw new Error('Not allowed');
+    return {
+      props: {
+        name,
+        isAdmin
+      }
+    };
+  } catch (err) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    };
+  }
+};
+
+export const getServerSidePropsUser: GetServerSideProps<unknown> = async (context: any) => {
+  try {
+    checkAuth(context.req.cookies['@my-school:token']);
+    const { id, name } = JSON.parse(context.req.cookies['@my-school:user']);
+    const isAdmin = await checkPermission(context.req.cookies['@my-school:token'], id);
     return {
       props: {
         name,
