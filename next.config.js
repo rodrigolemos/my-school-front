f3b45313677e981452e33070c29a7c27c5a7f877
Rodrigo <rodrigolemosrl@hotmail.com>
2021-07-17 16:36:43 -0300

    build: env variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withImages = require('next-images');
-module.exports = withImages();
+module.exports = {
+  ...withImages(),
+  env: {
+    BACKEND_URL: 'http://localhost:3000'
+  }
+};
